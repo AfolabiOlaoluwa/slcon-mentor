e5888682409387bd78e46f566711f640ca3837b9
Anthony Nandaa <profnandaa@gmail.com>
2015-12-30 22:40:49 +0300

    (feature) DB Schema v1.0

---
+++ design/db/Dump20151230.sql
@@ -0,0 +1,207 @@
+CREATE DATABASE  IF NOT EXISTS `slcon_mentor` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `slcon_mentor`;
+-- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
+--
+-- Host: 127.0.0.1    Database: slcon_mentor
+-- ------------------------------------------------------
+-- Server version	5.6.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `interest`
+--
+
+DROP TABLE IF EXISTS `interest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `interest` (
+  `iid` int(11) NOT NULL AUTO_INCREMENT,
+  `interest` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`iid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `link`
+--
+
+DROP TABLE IF EXISTS `link`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `link` (
+  `lid` int(11) NOT NULL AUTO_INCREMENT,
+  `ltid` int(11) DEFAULT NULL,
+  `mid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`lid`),
+  KEY `ltid` (`ltid`),
+  KEY `mid` (`mid`),
+  CONSTRAINT `link_ibfk_1` FOREIGN KEY (`ltid`) REFERENCES `link_type` (`ltid`),
+  CONSTRAINT `link_ibfk_2` FOREIGN KEY (`mid`) REFERENCES `member` (`mid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `link_type`
+--
+
+DROP TABLE IF EXISTS `link_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `link_type` (
+  `ltid` int(11) NOT NULL AUTO_INCREMENT,
+  `link_type` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ltid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `member` (
+  `mid` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(40) DEFAULT NULL,
+  `last_name` varchar(40) DEFAULT NULL,
+  `nationality` varchar(40) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`mid`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `member_interest`
+--
+
+DROP TABLE IF EXISTS `member_interest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `member_interest` (
+  `miid` int(11) NOT NULL AUTO_INCREMENT,
+  `iid` int(11) DEFAULT NULL,
+  `mid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`miid`),
+  KEY `iid` (`iid`),
+  KEY `mid` (`mid`),
+  CONSTRAINT `member_interest_ibfk_1` FOREIGN KEY (`iid`) REFERENCES `interest` (`iid`),
+  CONSTRAINT `member_interest_ibfk_2` FOREIGN KEY (`mid`) REFERENCES `member` (`mid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `member_skill`
+--
+
+DROP TABLE IF EXISTS `member_skill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `member_skill` (
+  `msid` int(11) NOT NULL AUTO_INCREMENT,
+  `mid` int(11) DEFAULT NULL,
+  `sid` int(11) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  PRIMARY KEY (`msid`),
+  KEY `mid` (`mid`),
+  KEY `sid` (`sid`),
+  CONSTRAINT `member_skill_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `member` (`mid`),
+  CONSTRAINT `member_skill_ibfk_2` FOREIGN KEY (`sid`) REFERENCES `skill` (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mentor`
+--
+
+DROP TABLE IF EXISTS `mentor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mentor` (
+  `mtid` int(11) NOT NULL,
+  `mentor_id` int(11) DEFAULT NULL,
+  `mentee_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`mtid`),
+  KEY `mentor_id` (`mentor_id`),
+  CONSTRAINT `mentor_ibfk_1` FOREIGN KEY (`mentor_id`) REFERENCES `member` (`mid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project` (
+  `pid` int(11) NOT NULL AUTO_INCREMENT,
+  `mid` int(11) DEFAULT NULL,
+  `project_name` varchar(100) DEFAULT NULL,
+  `descr` text,
+  `url` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`pid`),
+  KEY `mid` (`mid`),
+  CONSTRAINT `project_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `member` (`mid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `project_skill`
+--
+
+DROP TABLE IF EXISTS `project_skill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project_skill` (
+  `psid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) DEFAULT NULL,
+  `sid` int(11) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  PRIMARY KEY (`psid`),
+  KEY `pid` (`pid`),
+  KEY `sid` (`sid`),
+  CONSTRAINT `project_skill_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `project` (`pid`),
+  CONSTRAINT `project_skill_ibfk_2` FOREIGN KEY (`sid`) REFERENCES `skill` (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `skill`
+--
+
+DROP TABLE IF EXISTS `skill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `skill` (
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
+  `skill_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-30 22:28:50
