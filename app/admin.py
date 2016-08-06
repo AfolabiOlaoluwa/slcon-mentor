687d87c039f2347c015c76402eb1993104969582
Kevin Ndung'u <kevgathuku@gmail.com>
2016-08-06 11:05:35 +0300

    (chore) Add Mentorship to Admin
    Customize the Member model admin form

--- app/admin.py
+++ app/admin.py
@@ -3,12 +3,23 @@ from __future__ import absolute_import, unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Interest, Member, MemberLink, MemberSkill, Project, ProjectLink, Skill
+from .models import (Interest, Member, MemberLink, MemberSkill, Mentorship,
+                     Project, ProjectLink, Skill)
+
+
+class MemberAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('User information', {'fields': ['username', 'first_name', 'last_name', 'email', 'location']}),
+        ('Mentorship information', {'fields': ['role', 'interests']}),
+        ('Login information', {'fields': ['password', 'last_login']}),
+    ]
+
 
 admin.site.register(Interest)
-admin.site.register(Member)
+admin.site.register(Member, MemberAdmin)
 admin.site.register(MemberLink)
 admin.site.register(MemberSkill)
+admin.site.register(Mentorship)
 admin.site.register(Project)
 admin.site.register(ProjectLink)
 admin.site.register(Skill)
