32b67a79e4a76338ce2c55c73b6771edd9121407
andela-jngatia <joan.ngatia@andela.com>
2017-01-26 16:25:03 +0300

    [Chore] Split project settings on basis of environment

---
+++ slcon/settings/local.py
@@ -0,0 +1,6 @@
+# development-environment specific settings
+
+from .base import *
+
+ # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '^*68cz#^r#%q%@$wp$9c^&8vq#8b#@f_g0(&@coo9whvl@_br2'
---
+++ slcon/settings/test.py
@@ -0,0 +1,10 @@
+# testing-environment specific settings
+
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), 'test.db')
+    }
+}
